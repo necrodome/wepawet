cbaa43299769acbe169ca11ff919bd37c146864b
Chris Lee <rubygems@chrislee.dhs.org>
2011-04-24 17:30:09 -0400

    Regenerate gemspec for version 0.1.0

---
+++ lib/wepawet.rb
@@ -0,0 +1,1 @@
+require 'wepawet/wepawet'
--- test/test_wepawet.rb
+++ test/test_wepawet.rb
@@ -1,7 +1,23 @@
 require 'helper'
 
 class TestWepawet < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
+	should "submit submit http://example.com for analysis" do
+		config = {
+			'wepawetSubmitUrl' => 'http://wepawet.cs.ucsb.edu/services/upload.php', 
+			'wepawetQueryUrl' => 'http://wepawet.cs.ucsb.edu/services/query.php',
+			'wepawetDomainUrl' => 'http://wepawet.cs.ucsb.edu/services/domain.php',
+			'wepawetUrlUrl' => 'http://wepawet.cs.ucsb.edu/services/url.php',
+		}
+		w = Wepawet::Submit.new(config)
+		hash = w.submit_url("http://example.com")
+		assert_equal(32, hash.length)
+		assert(hash =~ /^[a-fA-F0-9]{32}$/)
+		q = Wepawet::Query.new(config)
+		resp = q.by_taskid(hash)
+		assert("http://example.com", resp['url'])
+		resp = q.by_domain("example.com")
+		assert("example.com", resp['domain'])
+		resp = q.by_url("http://example.com")
+		assert("http://example.com", resp['url'])
+	end
 end
